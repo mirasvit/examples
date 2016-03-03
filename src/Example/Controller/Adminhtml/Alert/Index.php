02790e82e95501282d33de2b02d27a0668da9909
vtruhmanov <vtruhmanov@mirasvit.com>
2016-03-03 19:48:25 +0200

    backend page

---
+++ src/Example/Controller/Adminhtml/Alert/Index.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Mirasvit\Example\Controller\Adminhtml\Alert;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Mirasvit_Example::example_alert');
+        $resultPage->getConfig()->getTitle()->prepend(__('Alert Widget Example'));
+
+        return $resultPage;
+    }
+
+    /**
+     * Is the user allowed to view the blog post grid.
+     *
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Mirasvit_Example::example_alert');
+    }
+
+
+}
