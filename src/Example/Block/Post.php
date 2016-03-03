a9c04ebda547213eed46000405b2d0b73a04da27
vtruhmanov <vtruhmanov@mirasvit.com>
2016-03-03 17:43:46 +0200

    Mirasvit Example Draft

---
+++ src/Example/Block/Post.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mirasvit\Example\Block;
+
+class Post extends \Magento\Framework\View\Element\Template
+{
+    public function _prepareLayout()
+    {
+        return parent::_prepareLayout();
+    }
+}
