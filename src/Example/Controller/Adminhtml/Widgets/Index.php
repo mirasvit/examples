ac797a406edaba92cdf8538b0a348ff3e887d601
vtruhmanov <vtruhmanov@mirasvit.com>
2016-03-07 19:13:51 +0200

    widget draft tests

---
+++ src/Example/Controller/Adminhtml/Widgets/Index.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mirasvit\Example\Controller\Adminhtml\Widgets;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+
+    /**
+     * @var PageFactory
+     */
+    protected $resultPageFactory;
+
+    /**
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Index action
+     *
+     * @return \Magento\Backend\Model\View\Result\Page
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Mirasvit_Example::example_widgets');
+
+        return $resultPage;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Mirasvit_Example::example_widgets');
+    }
+
+
+}
